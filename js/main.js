f35d20c12bbdbb065c30f7b223efc3f49f28276f
JethroNatividad <natividad.jet@gmail.com>
2024-03-02 16:12:18 +0800

    add dropdown

--- js/main.js
+++ js/main.js
@@ -2,3 +2,18 @@ document.querySelector("#write-post-btn").addEventListener("click", () => {
   // redirect to /create.html
   window.location.href = "/create.html";
 });
+
+const profile = document.querySelector(".profile");
+const dropdown = document.querySelector(".dropdown-menu");
+
+profile.addEventListener("click", (event) => {
+  // Toggle the 'show' class instead of directly manipulating the style
+  dropdown.classList.toggle("show");
+  event.stopPropagation();
+});
+
+document.addEventListener("click", () => {
+  if (dropdown.classList.contains("show")) {
+    dropdown.classList.remove("show");
+  }
+});
