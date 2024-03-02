c9de5e08f13b2e4c448c198fcfe9777645c2bfe8
JethroNatividad <natividad.jet@gmail.com>
2024-03-02 11:28:42 +0800

    style image preview

--- js/create.js
+++ js/create.js
@@ -1,11 +1,11 @@
 document
-  .getElementById("drop-area")
+  .querySelector("#drop-area")
   .addEventListener("dragover", function (event) {
     event.preventDefault();
     event.stopPropagation();
   });
 
-document.getElementById("drop-area").addEventListener("drop", function (event) {
+document.querySelector("#drop-area").addEventListener("drop", function (event) {
   event.preventDefault();
   event.stopPropagation();
   handleFiles(event.dataTransfer.files);
@@ -17,10 +17,10 @@ function handleFiles(files) {
     if (file.type.match(/image\/png|image\/jpeg|image\/webp/)) {
       const reader = new FileReader();
       reader.onloadend = function (e) {
-        document.getElementById("preview-img").style.backgroundImage =
+        document.querySelector("#preview-img").style.backgroundImage =
           "url(" + e.target.result + ")";
-        document.getElementById("preview").style.display = "block";
-        // document.getElementById("removeButton").style.display = "block"; // Show the remove button
+        document.querySelector("#preview").style.display = "block";
+        document.querySelector(".drop-area-label").style.display = "none";
       };
       reader.readAsDataURL(file);
     }
@@ -29,8 +29,8 @@ function handleFiles(files) {
 }
 
 // Add an event listener for the remove button
-document.getElementById("removeButton").addEventListener("click", function () {
-  document.getElementById("preview").style.backgroundImage = ""; // Clear the background image
-  document.getElementById("preview").style.display = "none"; // Hide the preview area
-  document.getElementById("removeButton").style.display = "none"; // Hide the remove button
+document.querySelector("#removeButton").addEventListener("click", function () {
+  document.querySelector("#preview-img").style.backgroundImage = ""; // Clear the background image
+  document.querySelector("#preview").style.display = "none"; // Hide the preview area
+  document.querySelector(".drop-area-label").style.display = "block";
 });
