7c8f864793f0f921221feb649968739e303d9585
JethroNatividad <natividad.jet@gmail.com>
2024-03-02 11:49:42 +0800

    add icon

--- js/create.js
+++ js/create.js
@@ -1,3 +1,8 @@
+const form = document.querySelector("form");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
+
 document
   .querySelector("#drop-area")
   .addEventListener("dragover", function (event) {
